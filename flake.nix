bcb833cc11ad73ff46bb7c216473d15c7fcf5cb8
Bernard Assan <mega.alpha100@gmail.com>
2025-09-14 23:15:38 +0000

    update CI to use Zig 0.15.1
    update flake.nix
    
    Co-authored-by: Muki <muki@muki.gg>
    
    Signed-off-by: Bernard Assan <mega.alpha100@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,24 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+
+    zigPkgs.url = "github:mitchellh/zig-overlay";
+    zigPkgs.inputs.nixpkgs.follows = "nixpkgs";
+
+    zlsPkg.url = "github:zigtools/zls/0.15.0";
+    zlsPkg.inputs.zig-overlay.follows = "zigPkgs";
+    zlsPkg.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
+      zigPkgs,
+      zlsPkg,
       flake-utils,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          (final: prev: {
+            zigpkgs = zigPkgs.packages.${prev.system};
+            zls = zlsPkg.packages.${prev.system}.default;
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            zig
+            zigpkgs."0.15.1"
             zls
             openssl
             inetutils
